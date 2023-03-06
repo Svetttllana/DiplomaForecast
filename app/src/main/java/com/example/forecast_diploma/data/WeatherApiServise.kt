dd124ddbbad43c51f8f2398b1c9ef0a8539b5430
Svetttllana <Szgirovskaya@mail.ru>
2023-03-07 00:22:00 +0300

    new api;add serializedName

--- app/src/main/java/com/example/forecast_diploma/data/WeatherApiServise.kt
+++ app/src/main/java/com/example/forecast_diploma/data/WeatherApiServise.kt
@@ -5,9 +5,6 @@ import retrofit2.Response
 import retrofit2.http.*
 
 
-//https://api.jsonserve.com/fXS861
-
-//https://api.jsonserve.com/5RMTpc
 //https://api.jsonserve.com/UrRXCP
 interface WeatherApiServise {
 
