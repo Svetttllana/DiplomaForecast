c8346a0e1eecd04f2c5a40982e4f4c0209ef54ae
Svetttllana <Szgirovskaya@mail.ru>
2023-03-09 13:35:42 +0300

    add search and currentFragment

---
+++ app/src/main/java/com/example/forecast_diploma/presentation/CurrentWeatherViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.forecast_diploma.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class CurrentWeatherViewModel @Inject constructor(
+) :ViewModel()
+{
+}
---
+++ app/src/main/java/com/example/forecast_diploma/utils/Constans.kt
@@ -0,0 +1,14 @@
+package com.example.forecast_diploma.utils
+
+object Constans {
+
+    const val TIME = "time"
+    const val NAME = "name"
+    const val COUNTRY = "country"
+    const val REGION = "region"
+    const val TEXT = "text"
+    const val TEMP_C = "temp_c"
+    const val ICON = "icon"
+
+}
+
